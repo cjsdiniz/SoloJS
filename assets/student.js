05cd029241e03ae28fde0801c75eb174ee1a2e3c
cjsdiniz <67067863+cjsdiniz@users.noreply.github.com>
2021-11-03 19:02:47 +0000

    Update student.js

--- assets/student.js
+++ assets/student.js
@@ -19,3 +19,14 @@ console.log(newStudent.xp); // 2
 // alert(newStudent.age); // 20
 // alert(newStudent.sex); // male
 // alert(newStudent.xp); // 2
+
+// let person = {
+//     name: 'Jack',
+//     age: 18
+// };
+
+let newPerson = person;
+newPerson.name = 'Bob';
+
+console.log(person.name); // Bob
+console.log(newPerson.name); // Bob
