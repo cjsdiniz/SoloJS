0f05c8e6f1e49da581ce7da18c00389b73ec68b5
cjsdiniz <67067863+cjsdiniz@users.noreply.github.com>
2021-11-03 13:20:43 +0000

    Student

---
+++ assets/student.js
@@ -0,0 +1,21 @@
+let person = {
+    name: 'Jack',
+    age: 18,
+    sex: 'male'
+};
+let student = {
+    name: 'Bob',
+    age: 20,
+    xp: '2'
+};
+
+let newStudent = Object.assign({}, person, student);
+
+console.log(newStudent.name); // Bob
+console.log(newStudent.age); // 20
+console.log(newStudent.sex); // male
+console.log(newStudent.xp); // 2
+// alert(newStudent.name); // Bob
+// alert(newStudent.age); // 20
+// alert(newStudent.sex); // male
+// alert(newStudent.xp); // 2
